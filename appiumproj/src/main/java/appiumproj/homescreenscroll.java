4b5eed0f96d8a20e76f6052199ffaee58aa7eee0
BizAct-108 <BizAct-108@BizAct108.bizacuity.local>
2020-12-15 14:12:48 +0530

    Initial messaga

---
+++ appiumproj/src/main/java/appiumproj/homescreenscroll.java
@@ -0,0 +1,72 @@
+package appiumproj;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.interactions.touch.TouchActions;
+
+public class homescreenscroll {
+
+  private AndroidDriver driver;
+
+  @Before
+  public void setUp() throws MalformedURLException {
+  	    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+  	    desiredCapabilities.setCapability("platformName", "Android");
+  	    desiredCapabilities.setCapability("platformVersion", "11.0");
+  	    desiredCapabilities.setCapability("deviceName", "Nexus 6");
+  	    desiredCapabilities.setCapability("app", "C:\\Expo.apk.apk");
+  	    desiredCapabilities.setCapability("appPackage", "host.exp.exponent");
+  	    desiredCapabilities.setCapability("appActivity", ".experience.HomeActivity");
+  	    desiredCapabilities.setCapability("automationName", "UiAutomator2");
+  	    desiredCapabilities.setCapability("ensureWebviewsHavePages", "true");
+
+  	    URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+
+  	    driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+  }
+
+  @Test
+  public void alltest() {
+	  new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Open from Clipboard']"))).click();
+	  new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Description']")));
+	  new TouchAction(driver).press(PointOption.point(870,781)).waitAction(WaitOptions.waitOptions(Duration.ofSeconds(30))).moveTo(PointOption.point(874,588)).release().perform();
+	  WebElement el12 = (new WebDriverWait(driver,60)).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Open Project Using Expo']")));
+	  el12.click();
+	  new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='English']")));
+      driver.findElement(By.xpath("//*[@text='English']")).click();
+      new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Got it']")));
+      driver.navigate().back();
+	  WebElement e14 = (new WebDriverWait(driver, 30)).until(ExpectedConditions.elementToBeClickable(By.xpath("MobileElement el2 = (MobileElement) driver.findElementByXPath(\"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup\");\r\n"
+		  		+ "el2.click();\r\n"
+		  		+ "MobileElement el3 = (MobileElement) driver.findElementByXPath(\"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.EditText")));
+		  e14.sendKeys("7036780100");  //your number
+      driver.findElement(By.xpath("//*[@text='SEND OTP']")).click();
+      WebElement e16 = (new WebDriverWait(driver, 30)).until(ExpectedConditions.elementToBeClickable(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.EditText")));
+	  e16.sendKeys("1001"); //OTP override
+	  new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup/android.view.ViewGroup"))).click();
+	  new TouchAction(driver).press(PointOption.point(870,781)).waitAction(WaitOptions.waitOptions(Duration.ofSeconds(30))).moveTo(PointOption.point(874,588)).release().perform();
+	  driver.navigate().back();
+  }
+  @After
+  public void tearDown() {
+     driver.quit();   
+  }
+}
---
+++ appiumproj/src/main/java/appiumproj/notifications.java
@@ -0,0 +1,73 @@
+package appiumproj;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.interactions.touch.TouchActions;
+
+public class notifications {
+
+  private AndroidDriver driver;
+
+  @Before
+  public void setUp() throws MalformedURLException {
+  	    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+  	    desiredCapabilities.setCapability("platformName", "Android");
+  	    desiredCapabilities.setCapability("platformVersion", "11.0");
+  	    desiredCapabilities.setCapability("deviceName", "Nexus 6");
+  	    desiredCapabilities.setCapability("app", "C:\\Expo.apk.apk");
+  	    desiredCapabilities.setCapability("appPackage", "host.exp.exponent");
+  	    desiredCapabilities.setCapability("appActivity", ".experience.HomeActivity");
+  	    desiredCapabilities.setCapability("automationName", "UiAutomator2");
+  	    desiredCapabilities.setCapability("ensureWebviewsHavePages", "true");
+
+  	    URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+
+  	    driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+  }
+
+  @Test
+  public void alltest() {
+	  new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Open from Clipboard']"))).click();
+	  new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Description']")));
+	  new TouchAction(driver).press(PointOption.point(870,781)).waitAction(WaitOptions.waitOptions(Duration.ofSeconds(30))).moveTo(PointOption.point(874,588)).release().perform();
+	  WebElement el12 = (new WebDriverWait(driver,60)).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@text='Open Project Using Expo']")));
+	  el12.click();
+	  new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='English']")));
+      driver.findElement(By.xpath("//*[@text='English']")).click();
+      new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Got it']")));
+      driver.navigate().back();
+	  WebElement e14 = (new WebDriverWait(driver, 30)).until(ExpectedConditions.elementToBeClickable(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.EditText")));
+	  e14.sendKeys("7036780100");  //your number
+      driver.findElement(By.xpath("//*[@text='SEND OTP']")).click();
+      WebElement e16 = (new WebDriverWait(driver, 30)).until(ExpectedConditions.elementToBeClickable(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.EditText")));
+	  e16.sendKeys("1001"); //OTP override
+	  WebElement ef1 = (new WebDriverWait(driver, 30)).until(ExpectedConditions.elementToBeClickable(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup")));
+	  ef1.click(); //menu
+	  new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Notifications']")));
+      driver.findElement(By.xpath("//*[@text='Notifications']")).click();
+      new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Hi Mukesh, We have received your grievance with no. #28696 for category of Disabled Welfare. For further updates, please check the Nivedhana app.']")));
+      driver.navigate().back();
+  }
+  @After
+  public void tearDown() {
+     driver.quit();   
+  }
+}
